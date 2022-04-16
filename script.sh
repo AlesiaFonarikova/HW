2eaed1473027119168ff6b6798e221d4ed3c6089
AlesiaFonarikova <afonarikova@gmail.com>
2022-04-16 14:49:05 +0300

    Terminal Linux HW №1

---
+++ script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+		mkdir hwork_gitbash;
+		cd hwork_gitbash;
+		mkdir folder_1 folder_2 folder_3;
+		cd folder_1;
+		touch f1.txt f2.txt f3.txt f4.json f5.json;
+		mkdir gitbash_1 gitbash_2 gitbash_3;
+		ls -la ./hwork_gitbash/folder_1;
+		mv {f1.txt,f2.txt} gitbash_1/;
